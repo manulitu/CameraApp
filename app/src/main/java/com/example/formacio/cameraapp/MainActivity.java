bbcbd93bdb19188ee47b1314232dfde628ad35e2
unknown <test.456@gmail.com>
2018-07-13 09:49:04 +0200

    Friday 13th July 2018

--- app/src/main/java/com/example/formacio/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/formacio/cameraapp/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.formacio.cameraapp;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private Button pictureButton;
     private ImageView imageView;
+    File pictureFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +34,25 @@ public class MainActivity extends AppCompatActivity {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             pictureButton.setEnabled(false);
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
-
-
-
-
         }
 
 
+    }
+    @Override
+    public void OnRequestPermissionsResult(int requestCode, String [] permissions, int [] grantResults){
+        if(requestCode==0){
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                pictureButton.setEnabled(true);
+            }
+        }
+    }
+
+    public void takePicture(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        pictureFile = Uri.fromFile(getOutputMediaFile());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, pictureFile);
+
+        startActivityForResult(intent, 100);
     }
 }
